1a8e2b93a2d72436d785bca8b50042825832b3dc
Kaushika024 <94302959+Kaushika024@users.noreply.github.com>
2021-11-24 22:57:50 +0530

    Update CafeteriaManagement.h

--- 3_Implementation/inc/CafeteriaManagement.h
+++ 3_Implementation/inc/CafeteriaManagement.h
@@ -1,1 +1,13 @@
+/** 
+* @file CafeteriaManagement.h
+* Cafeteria billing application
+*
+*/
+#ifndef __CAFETERIA_MANAGEMENT__H_
+#define __CAFETERIA_MANAGEMENT_H__
 
+#include<stdio.h>
+#include<stdlib.h>
+
+ 
+#endif  /* #define __CAFETERIA_MANAGEMENT_H__ */
